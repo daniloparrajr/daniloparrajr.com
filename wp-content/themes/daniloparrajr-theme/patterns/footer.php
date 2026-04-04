199a67f9ecc574fb3195c01fbae30c95a69f6e64
Danilo Parra Jr. <daniloparrajr.work@gmail.com>
2026-04-04 12:11:15 +0800

    feat(theme): add footer template part

---
+++ wp-content/themes/daniloparrajr-theme/patterns/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: footer
+ * Slug: daniloparrajr-theme/footer
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Call to action"},"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|blue-black-50"}}},"spacing":{"padding":{"top":"var:preset|spacing|fluid-1000","bottom":"var:preset|spacing|fluid-1000","left":"var:preset|spacing|600","right":"var:preset|spacing|600"}},"border":{"radius":{"topLeft":"40px","topRight":"40px"}}},"backgroundColor":"black-700","textColor":"blue-black-50","layout":{"type":"constrained","contentSize":"700px","wideSize":"500px"}} -->
+<div class="wp-block-group alignwide has-blue-black-50-color has-black-700-background-color has-text-color has-background has-link-color" style="border-top-left-radius:40px;border-top-right-radius:40px;padding-top:var(--wp--preset--spacing--fluid-1000);padding-right:var(--wp--preset--spacing--600);padding-bottom:var(--wp--preset--spacing--fluid-1000);padding-left:var(--wp--preset--spacing--600)"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<p class="has-text-align-center" style="font-style:normal;font-weight:600"><?php esc_html_e('Connect', 'daniloparrajr-theme');?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|blue-black-100"}}}},"textColor":"blue-black-100","fontSize":"display-medium"} -->
+<h2 class="wp-block-heading has-text-align-center has-blue-black-100-color has-text-color has-link-color has-display-medium-font-size"><?php esc_html_e('Let\'s create your next big idea.', 'daniloparrajr-theme');?></h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline","style":{"spacing":{"padding":{"left":"var:preset|spacing|fluid-400","right":"var:preset|spacing|fluid-400","top":"var:preset|spacing|400","bottom":"var:preset|spacing|400"}}},"fontSize":"body-medium"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-body-medium-font-size has-custom-font-size wp-element-button" style="padding-top:var(--wp--preset--spacing--400);padding-right:var(--wp--preset--spacing--fluid-400);padding-bottom:var(--wp--preset--spacing--400);padding-left:var(--wp--preset--spacing--fluid-400)"><?php esc_html_e('Get in touch', 'daniloparrajr-theme');?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|fluid-200","bottom":"var:preset|spacing|fluid-200","left":"var:preset|spacing|600","right":"var:preset|spacing|600"}},"elements":{"link":{"color":{"text":"var:preset|color|blue-black-100"}}},"border":{"radius":{"bottomLeft":"40px","bottomRight":"40px"}}},"backgroundColor":"black-700","textColor":"blue-black-100","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide has-blue-black-100-color has-black-700-background-color has-text-color has-background has-link-color" style="border-bottom-left-radius:40px;border-bottom-right-radius:40px;padding-top:var(--wp--preset--spacing--fluid-200);padding-right:var(--wp--preset--spacing--600);padding-bottom:var(--wp--preset--spacing--fluid-200);padding-left:var(--wp--preset--spacing--600)"><!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","justifyContent":"center"}} /-->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php esc_html_e('© 2026 Danilo Parra Jr.', 'daniloparrajr-theme');?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
