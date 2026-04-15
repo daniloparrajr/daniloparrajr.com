87f588539031fcb7e07ec996f98c631b244ddce2
Danilo Parra Jr. <daniloparrajr.work@gmail.com>
2026-04-15 14:30:39 +0800

    fix(theme): update footer font sizes

--- wp-content/themes/daniloparrajr-theme/patterns/footer.php
+++ wp-content/themes/daniloparrajr-theme/patterns/footer.php
@@ -8,8 +8,8 @@
 <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Call to action"},"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|blue-black-50"}}},"spacing":{"padding":{"top":"var:preset|spacing|fluid-1000","bottom":"var:preset|spacing|fluid-1000","left":"var:preset|spacing|600","right":"var:preset|spacing|600"}},"border":{"radius":{"topLeft":"40px","topRight":"40px"}}},"backgroundColor":"black-700","textColor":"blue-black-50","layout":{"type":"constrained","contentSize":"700px","wideSize":"500px"}} -->
 <div class="wp-block-group alignwide has-blue-black-50-color has-black-700-background-color has-text-color has-background has-link-color" style="border-top-left-radius:40px;border-top-right-radius:40px;padding-top:var(--wp--preset--spacing--fluid-1000);padding-right:var(--wp--preset--spacing--600);padding-bottom:var(--wp--preset--spacing--fluid-1000);padding-left:var(--wp--preset--spacing--600)"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
-<p class="has-text-align-center" style="font-style:normal;font-weight:600"><?php esc_html_e('Connect', 'daniloparrajr-theme');?></p>
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600","textTransform":"uppercase","letterSpacing":"2px"}},"fontSize":"text-6"} -->
+<p class="has-text-align-center has-text-6-font-size" style="font-style:normal;font-weight:600;letter-spacing:2px;text-transform:uppercase"><?php esc_html_e('Connect', 'daniloparrajr-theme');?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|blue-black-100"}}}},"textColor":"blue-black-100","fontSize":"headline-1"} -->
@@ -27,8 +27,8 @@
 <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|fluid-200","bottom":"var:preset|spacing|fluid-200","left":"var:preset|spacing|600","right":"var:preset|spacing|600"}},"elements":{"link":{"color":{"text":"var:preset|color|blue-black-100"}}},"border":{"radius":{"bottomLeft":"40px","bottomRight":"40px"}}},"backgroundColor":"black-700","textColor":"blue-black-100","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide has-blue-black-100-color has-black-700-background-color has-text-color has-background has-link-color" style="border-bottom-left-radius:40px;border-bottom-right-radius:40px;padding-top:var(--wp--preset--spacing--fluid-200);padding-right:var(--wp--preset--spacing--600);padding-bottom:var(--wp--preset--spacing--fluid-200);padding-left:var(--wp--preset--spacing--600)"><!-- wp:navigation {"textColor":"black-100","overlayMenu":"never","layout":{"type":"flex","justifyContent":"center"}} /-->
 
-<!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center"><?php esc_html_e('© 2026 Danilo Parra Jr.', 'daniloparrajr-theme');?></p>
+<!-- wp:paragraph {"align":"center","fontSize":"text-6"} -->
+<p class="has-text-align-center has-text-6-font-size"><?php esc_html_e('© 2026 Danilo Parra Jr.', 'daniloparrajr-theme');?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
